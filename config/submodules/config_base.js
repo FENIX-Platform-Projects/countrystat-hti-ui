148bd8af661538b0a030d305c5d523c330743dc5
fabriziofenix <fabrizio.castelli@fao.org>
2016-04-13 19:20:40 +0200

    configured

--- config/submodules/config_base.js
+++ config/submodules/config_base.js
@@ -7,11 +7,11 @@ define([], function () {
     //services.SERVICE_BASE_ADDRESS = "http://fenix.fao.org/d3s_dev2/msd";
     var services = {
         //Prod server
-        //SERVICE_BASE_ADDRESS: "http://fenixservices.fao.org/d3s/msd"
+        //SERVICE_BASE_ADDRESS: "http://fenix.fao.org/d3s/msd"
         //SERVICE_BASE_ADDRESS: 'http://fenix.fao.org/d3s_fenix/msd'
         //SERVICE_BASE_ADDRESS: "http://fenix.fao.org/d3s_dev/msd"
         //demo
-        SERVICE_BASE_ADDRESS: "http://fenixservices.fao.org/d3s/msd",
+        SERVICE_BASE_ADDRESS: "http://fenix.fao.org/d3s/msd",
         DSD_EDITOR_CODELISTS: "config/submodules/DSDEditor/CodelistsCstatZMB.json",
     };
 
